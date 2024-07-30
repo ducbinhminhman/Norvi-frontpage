ada843d13981328736fbff5506b34f647037d6fd
ducbinhminhman <manbinhminh99@gmail.com>
2024-07-30 16:42:46 +0200

    Update the navigation bar web

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,32 +5,38 @@ import { navLinks } from "../constants";
 const Nav = () => {
   return (
     <header className='padding-x py-8 absolute z-10 w-full'>
-      <nav className='flex justify-between items-center max-container'>
-        <a href='/'>
-          <img
-            src={headerLogo}
-            alt='logo'
-            width={129}
-            height={29}
-            className='m-0 w-[129px] h-[100px]'
-          />
-        </a>
-        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
-          {navLinks.map((item) => (
-            <li key={item.label}>
-              <a
-                href={item.href}
-                className='font-montserrat leading-normal text-lg text-slate-gray'
-              >
-                {item.label}
-              </a>
-            </li>
-          ))}
-        </ul>
-        <div className='hidden max-lg:block'>
-          <img src={hamburger} alt='hamburger icon' width={25} height={25} />
-        </div>
-      </nav>
+      <div className='max-container'>
+        <nav className='flex justify-between items-center'>
+          <div className='logo-container'>
+            <a href='/'>
+              <img
+                src={headerLogo}
+                alt='logo'
+                width={129}
+                height={29}
+                className='m-0 w-[129px] h-[100px]'
+              />
+            </a>
+          </div>
+          <div className='nav-links-container flex w-1/2 ml-auto'>
+            <ul className='flex justify-center items-center gap-16 max-lg:hidden'>
+              {navLinks.map((item) => (
+                <li key={item.label}>
+                  <a
+                    href={item.href}
+                    className='font-montserrat leading-normal text-lg text-slate-gray'
+                  >
+                    {item.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='hamburger-container hidden max-lg:block'>
+            <img src={hamburger} alt='hamburger icon' width={25} height={25} />
+          </div>
+        </nav>
+      </div>
     </header>
   );
 };
